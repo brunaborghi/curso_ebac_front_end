5ecb7d34faf773f4496c43d4fef3da8ecf668ac6
Bruna <118375033+brunaborghi@users.noreply.github.com>
2023-03-28 08:08:09 -0300

    Update main.js

--- main.js
+++ main.js
@@ -4,10 +4,12 @@ $(document).ready(function() {
    $('form').on('submit', function(e) { 
        e.preventDefault(); 
     
-       const novaTarefa = $('novaTarefa').val();
-       $('<li></li>').text(novaTarefa).appendTo('ul').click(novaTarefa);
-        console.log(novaTarefa);
+       const novaTarefa = $('#novaTarefa').val();
+       $('<li></li>').text(novaTarefa).appendTo('ul').click(() =>
+       $(event.target).css('text-decoration', 'line-through'))
+       $('#novaTarefa').val('');
+       
+       });
 
+      
    })
-
-})
