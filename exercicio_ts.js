5e5721b9f41eabcfa96f687ab25e8a5189415b7c
Bruna <118375033+brunaborghi@users.noreply.github.com>
2023-07-27 16:05:20 -0300

    Add files via upload

---
+++ exercicio_ts.js
@@ -0,0 +1,12 @@
+"use strict";
+function multiplicacao(a, b) {
+    return a * b;
+}
+const resultado = multiplicacao(6, 10);
+console.log(resultado);
+function dizOla(nome) {
+    return `Olá, ${nome}!`;
+}
+const nomes = "Bruna";
+const saudacao = dizOla(nomes);
+console.log(saudacao);
---
+++ exercicio_ts.ts
@@ -0,0 +1,18 @@
+function multiplicacao(a: number, b: number): number {
+    return a * b;
+  }
+
+  const resultado = multiplicacao(6,10);
+
+  console.log(resultado);
+
+  
+
+  function dizOla(nome: string): string {
+    return `Olá, ${nome}!`;
+  }
+
+  const nomes = "Bruna";
+  const saudacao = dizOla(nomes);
+
+  console.log(saudacao);
